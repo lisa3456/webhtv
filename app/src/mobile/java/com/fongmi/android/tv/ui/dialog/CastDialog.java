1f7144abef421d7ca3b834dd2357a67d250d1a84
youyou <93966747+lisa3456@users.noreply.github.com>
2026-08-17 15:36:45 +0800

    Add files via upload

--- app/src/mobile/java/com/fongmi/android/tv/ui/dialog/CastDialog.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/dialog/CastDialog.java
@@ -3,6 +3,7 @@ package com.fongmi.android.tv.ui.dialog;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
