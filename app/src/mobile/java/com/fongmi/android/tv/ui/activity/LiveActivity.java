35a1b9a229b39bbf569199116deeffac7c83f552
youyou <93966747+lisa3456@users.noreply.github.com>
2026-08-25 10:00:31 +0800

    Add files via upload

--- app/src/mobile/java/com/fongmi/android/tv/ui/activity/LiveActivity.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/activity/LiveActivity.java
@@ -309,9 +309,9 @@ public class LiveActivity extends PlaybackActivity implements CustomKeyDown.List
             mBinding.liveSetting.setOnClickListener(view -> onLiveSetting());
             mBinding.liveSetting.setOnTouchListener(this::onLiveSettingTouch);
         }
-        if (mBinding.liveCurrent != null) mBinding.liveCurrent.setOnClickListener(view -> onLiveProgram());
-        if (mBinding.liveProgram != null) mBinding.liveProgram.setOnClickListener(view -> onLiveProgram());
-        if (mBinding.liveProgramNext != null) mBinding.liveProgramNext.setOnClickListener(view -> onLiveProgram());
+        if (mBinding.liveCurrent != null) mBinding.liveCurrent.setOnClickListener(v -> onLine());
+        if (mBinding.liveProgram != null) mBinding.liveProgram.setOnClickListener(v -> onLine());
+        if (mBinding.liveProgramNext != null) mBinding.liveProgramNext.setOnClickListener(v -> onLine());
         mBinding.video.setOnTouchListener((view, event) -> mKeyDown.onTouchEvent(event));
     }
 
